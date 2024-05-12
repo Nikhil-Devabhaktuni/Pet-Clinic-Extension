a0f29f59f99a8aa7bf077f0a899489748e2fa6b8
Nikhil-Devabhaktuni <nikhildevabhaktuni@outlook.com>
2024-05-12 21:24:14 +0100

    Created_Project with Spring.io

---
+++ Pet-Clinic-Extension/src/main/java/gsf/petclinicextension/PetClinicExtensionApplication.java
@@ -0,0 +1,13 @@
+package gsf.petclinicextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicExtensionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicExtensionApplication.class, args);
+    }
+
+}
