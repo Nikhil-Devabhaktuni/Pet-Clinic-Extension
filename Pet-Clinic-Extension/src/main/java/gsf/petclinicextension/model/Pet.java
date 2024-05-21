84b5d79d3eaa2248145f6aeb686ef1d1af88d829
Nikhil-Devabhaktuni <nikhildevabhaktuni@outlook.com>
2024-05-21 19:20:32 +0100

    Just tried to create new Module
    Ignore Pussh

--- Pet-Clinic-Extension/src/main/java/gsf/petclinicextension/model/Pet.java
+++ Pet-Clinic-Extension/src/main/java/gsf/petclinicextension/model/Pet.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Pet
 {
     private PetType petType;
-    private Owner  owner;
+    private Owner owner;
     private LocalDate birthDate;
 
     public PetType getPetType() {
--- Pet-Clinic-Extension/src/main/java/gsf/petclinicextension/model/Vet.java
+++ Pet-Clinic-Extension/src/main/java/gsf/petclinicextension/model/Vet.java
@@ -1,4 +1,4 @@
 package gsf.petclinicextension.model;
 
-public class Vet extends Person{
+public class Vet extends Person {
 }
