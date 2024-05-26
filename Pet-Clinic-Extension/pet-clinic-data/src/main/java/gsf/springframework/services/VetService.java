940d16dad1764bbb55b58f361605aeb3fe59f38e
Nikhil-Devabhaktuni <nikhildevabhaktuni@outlook.com>
2024-05-26 22:22:10 +0100

    Refactoring to common interface
    Instead of using same boiler plate code services all the time created common interface which is curd service . closes #11

--- Pet-Clinic-Extension/pet-clinic-data/src/main/java/gsf/springframework/services/VetService.java
+++ Pet-Clinic-Extension/pet-clinic-data/src/main/java/gsf/springframework/services/VetService.java
@@ -4,11 +4,9 @@ import gsf.springframework.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 
     Vet findById(Long id);
 
-    Vet save(Vet vet);
 
-    Set<Vet> findAll();
 }
