5c348ac8fd36a9d887d897b137297fe2f3745865
Nikhil-Devabhaktuni <nikhildevabhaktuni@outlook.com>
2024-05-29 22:25:34 +0100

    List all Vets on Vet Index page
    Same like previous issue 17 , Created an list of vets index page and modified controller according to requirements. This will closes the issue 16. closes  #16 .

--- Pet-Clinic-Extension/pet-clinic-web/src/main/java/gsf/springframework/Controllers/VetController.java
+++ Pet-Clinic-Extension/pet-clinic-web/src/main/java/gsf/springframework/Controllers/VetController.java
@@ -1,14 +1,32 @@
 package gsf.springframework.Controllers;
 
+import gsf.springframework.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"vetIndex"})
     public String vetIndex()
     {
         return "vets/vetIndex";
+    }
+
+
+    @RequestMapping({"vetList"})
+    public String vetList(Model model)
+    {
+        model.addAttribute("vet",vetService.findAll());
+
+        return "vets/vetList";
     }
 }
