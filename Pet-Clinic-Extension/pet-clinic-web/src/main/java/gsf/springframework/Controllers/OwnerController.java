8f3469687f3ecbebde9e46cd61155e093552e30f
Nikhil-Devabhaktuni <nikhildevabhaktuni@outlook.com>
2024-05-29 22:04:45 +0100

    List Owners on Owners index page
    Just implemented the list of owners html page and added controller with auto setup bean via constructor injection to the Spring IOC container or spring Framework. This will close the issue no 17. closes #17 .

--- Pet-Clinic-Extension/pet-clinic-web/src/main/java/gsf/springframework/Controllers/OwnerController.java
+++ Pet-Clinic-Extension/pet-clinic-web/src/main/java/gsf/springframework/Controllers/OwnerController.java
@@ -1,14 +1,31 @@
 package gsf.springframework.Controllers;
 
+import gsf.springframework.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"owners"})
 @Controller
 public class OwnerController {
 
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"index"})
     public String OwnerIndex() {
         return "owner/ownerIndex";
+    }
+
+    @RequestMapping({"list"})
+    public String listOwners(Model model)
+    {
+        model.addAttribute("owners",ownerService.findAll()); // sending attribute to the frontend
+
+        return "owner/listOwners";
     }
 }
