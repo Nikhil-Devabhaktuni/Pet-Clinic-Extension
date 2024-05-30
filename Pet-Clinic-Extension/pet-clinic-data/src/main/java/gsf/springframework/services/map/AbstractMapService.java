6d9ed86da786e280385aeae450426bbd2be9a8bc
Nikhil-Devabhaktuni <nikhildevabhaktuni@outlook.com>
2024-05-30 19:07:34 +0100

    Update Map implementation to manage settings of ID property
    Created the automatic generate ID method instead of manually giving values to it , And refactored save method according to that. This is related to issue 24 created in Git. Closes #24 .

--- Pet-Clinic-Extension/pet-clinic-data/src/main/java/gsf/springframework/services/map/AbstractMapService.java
+++ Pet-Clinic-Extension/pet-clinic-data/src/main/java/gsf/springframework/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package gsf.springframework.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import gsf.springframework.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map=new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map=new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -17,8 +16,20 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object){
+
+        if(object!=null)
+        {
+            if(object.getId()==null)
+                {
+                    object.setId(getNextID());
+                }
+                map.put(object.getId(), object);
+        }
+        else
+        {
+            throw new RuntimeException("Object Cannot be null");
+        }
 
         return object;
     }
@@ -30,5 +41,18 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object){
         map.entrySet().removeIf(idtEntry -> idtEntry.getValue().equals(object));
+    }
+
+    private Long getNextID(){
+        Long nextID=null;
+
+        try{
+            nextID= Collections.max(map.keySet())+1;
+        }
+        catch (NoSuchElementException e)
+        {
+            nextID=1L;
+        }
+        return nextID;
     }
 }
--- Pet-Clinic-Extension/pet-clinic-web/src/main/java/gsf/springframework/bootstrap/DataLoader.java
+++ Pet-Clinic-Extension/pet-clinic-web/src/main/java/gsf/springframework/bootstrap/DataLoader.java
@@ -1,13 +1,9 @@
 package gsf.springframework.bootstrap;
-
-import ch.qos.logback.core.CoreConstants;
-import ch.qos.logback.core.net.SyslogOutputStream;
 import gsf.springframework.model.Owner;
 import gsf.springframework.model.Vet;
 import gsf.springframework.services.OwnerService;
 import gsf.springframework.services.VetService;
-import gsf.springframework.services.map.OwnerServiceMap;
-import gsf.springframework.services.map.VetServiceMap;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -31,14 +27,12 @@ public class DataLoader implements CommandLineRunner
         Owner owner1 = new Owner();
         owner1.setFirstName("John");
         owner1.setLastName("Wick");
-        owner1.setId(1L);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Dua");
         owner2.setLastName("Lipa");
-        owner2.setId(2L);
         ownerService.save(owner2);
 
         System.out.println("owners..... loading");
@@ -46,20 +40,17 @@ public class DataLoader implements CommandLineRunner
         Vet vet1 = new Vet();
         vet1.setFirstName("Nikhil");
         vet1.setLastName("Devabhaktuni");
-        vet1.setId(1L);
         vetService.save(vet1);
 
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jane");
         vet2.setLastName("D");
-        vet2.setId(2L);
         vetService.save(vet2);
 
         Vet vet3 = new Vet();
         vet3.setFirstName("Jesus");
         vet3.setLastName("God");
-        vet3.setId(3L);
         vetService.save(vet3);
 
         System.out.println("vets..... loading");
